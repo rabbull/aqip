66f725fcac635637a4e563581a8d72468be9fb48
DrewS <1251956930@qq.com>
2020-10-11 16:08:17 +0800

    Use nn.ModuleList to replace the one-by-one defined layers

--- model/AQIP.py
+++ model/AQIP.py
@@ -11,29 +11,22 @@ class AQIP(nn.Module):
         super().__init__()
         self.hid_size = 128
         self.seq_len = seq_len
-        # self.gat_layers = [
-        #     GATLayer(input_dim=16 + int(with_aqi), output_dim=128, adj=adj).cuda(),
-        #     GATLayer(input_dim=128, output_dim=128, adj=adj).cuda(),
-        # ]
-        self.GAT_1 = GATLayer(input_dim=16 + int(with_aqi), output_dim=128, adj=adj)
-        self.GAT_2 = GATLayer(input_dim=128, output_dim=128, adj=adj)
-        # self.rnns = [
-        #     nn.LSTM(input_size=128, hidden_size=128, num_layers=4, bias=True, batch_first=True).cuda(),
-        # ]
-        self.RNN_1 = nn.LSTM(input_size=128, hidden_size=128, num_layers=4, bias=True, batch_first=True)
+        self.gat_layers = nn.ModuleList([
+            GATLayer(input_dim=16 + int(with_aqi), output_dim=128, adj=adj).cuda(),
+            GATLayer(input_dim=128, output_dim=128, adj=adj).cuda(),
+        ])
+        self.rnns = nn.ModuleList([
+            nn.LSTM(input_size=128, hidden_size=128, num_layers=4, bias=True, batch_first=True).cuda(),
+        ])
         self.linear = nn.Linear(in_features=128 * 4, out_features=1, bias=True)
-        #self.linear = nn.Linear(in_features=128, out_features=1, bias=True)
 
     def forward(self, x: torch.Tensor, site_idx: int):
         h = torch.zeros(size=(4, x.size(0), 128)).cuda()
         c = torch.zeros(size=(4, x.size(0), 128)).cuda()
-        # for gat in self.gat_layers:
-        #     x = gat(x)
-        x = self.GAT_1(x)
-        x = self.GAT_2(x)
-        # for rnn in self.rnns:
-        #     x[:, :, site_idx, :], (h, c) = rnn(x[:, :, site_idx, :], (h, c))
-        x[:, :, site_idx, :], (h, c) = self.RNN_1(x[:, :, site_idx, :], (h, c))
+        for gat in self.gat_layers:
+            x = gat(x)
+        for rnn in self.rnns:
+            x[:, :, site_idx, :], (h, c) = rnn(x[:, :, site_idx, :], (h, c))
         h = h.permute(1, 0, 2)
         h = h.reshape(h.size(0), -1)
         return self.linear(h).squeeze()
