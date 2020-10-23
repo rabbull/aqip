ead62f1141832ec71f3fdd3bf67086c39f4acc18
DrewS <1251956930@qq.com>
2020-10-23 10:14:24 +0800

    Bug found

--- model/AQIP.py
+++ model/AQIP.py
@@ -10,20 +10,23 @@ class AQIP(nn.Module):
     def __init__(self, adj: torch.tensor, seq_len: int, with_aqi: bool = True):
         super().__init__()
         self.hid_size = 128
+        # TODO Change hid_size to 64 cause Error of:
+        #  The expanded size of the tensor (128) must match the existing size (64) at non-singleton dimension 2
         self.seq_len = seq_len
         self.gat_layers = nn.ModuleList([
             GATLayer(input_dim=16 + int(with_aqi), output_dim=128, adj=adj),
             GATLayer(input_dim=128, output_dim=128, adj=adj),
         ])
         self.rnns = nn.ModuleList([
-            nn.LSTM(input_size=128, hidden_size=128, num_layers=4, bias=True, batch_first=True),
+            nn.LSTM(input_size=128, hidden_size=self.hid_size, num_layers=4, bias=True, batch_first=True),
+            nn.LSTM(input_size=128, hidden_size=self.hid_size, num_layers=4, bias=True, batch_first=True)
         ])
         self.linear = nn.Linear(in_features=128 * 4, out_features=1, bias=True)
 
     def forward(self, x: torch.Tensor, site_idx: int):
         torch.autograd.set_detect_anomaly(True)
-        h = torch.zeros(size=(4, x.size(0), 128)).cuda()
-        c = torch.zeros(size=(4, x.size(0), 128)).cuda()
+        h = torch.zeros(size=(4, x.size(0), self.hid_size)).cuda()
+        c = torch.zeros(size=(4, x.size(0), self.hid_size)).cuda()
         for gat in self.gat_layers:
             x = gat(x)
         for rnn in self.rnns:
